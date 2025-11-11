ad6ad6e8f87a61c9e03dfb2fcfdeafca55f09e95
sayed.hussainullah <sayedsadat344@gmail.com>
2025-11-11 15:08:14 +0430

    project initiated

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+
+  console.log("Variable: ",import.meta.env.VITE_APPWRITE_ENDPOINT);
+  
+
+
+  return (
+    <>
+        <h1>React - AppWrite Blog</h1>
+    </>
+  )
+}
+
+export default App
